cd06a04324a785016caf677ba0af5884e189e8af
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-11-08 16:50:10 +0300

    Update main.js file

--- main.js
+++ main.js
@@ -3,14 +3,12 @@ const sideBar = document.querySelector('.sideBar');
 const sideBarExit = document.querySelector('.sideBar-exit');
 
 function openSidebar() {
-    // sideBar.style.display = 'visible';
     sideBar.classList.add('showSideBar');
 }
 
 humberger.addEventListener('click', openSidebar);
 
 function closeSidebar () {
-    // sideBar.style.display = 'hidden';
     sideBar.classList.remove('showSideBar');
 }
 
