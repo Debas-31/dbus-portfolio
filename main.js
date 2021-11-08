d9c9498c209512c4019cd104998d26f4146c7d03
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-11-08 23:02:15 +0300

    Update README file

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const humberger = document.querySelector('.humberger');
 const sideBar = document.querySelector('.sideBar');
 const sideBarExit = document.querySelector('.sideBar-exit');
+const sideBarLink = document.querySelector('.sideBar-link');
+
 
 function openSidebar() {
     sideBar.classList.add('showSideBar');
@@ -12,4 +14,5 @@ function closeSidebar () {
     sideBar.classList.remove('showSideBar');
 }
 
-sideBarExit.addEventListener('click', closeSidebar)
+sideBarExit.addEventListener('click', closeSidebar);
+sideBarLink.addEventListener('click', closeSidebar);
