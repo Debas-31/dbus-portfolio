19250e0d7e97813faa1553294f8b6ef201327df9
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-11-09 00:40:25 +0300

    Correct linters error

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const humberger = document.querySelector('.humberger');
 const sideBar = document.querySelector('.sideBar');
 const sideBarExit = document.querySelector('.sideBar-exit');
-const sideBarLink = document.querySelector('.sideBar-link');
-
+const sideBarLink = document.querySelectorAll('.sideBar-link');
 
 function openSidebar() {
-    sideBar.classList.add('showSideBar');
+  sideBar.classList.add('showSideBar');
 }
 
 humberger.addEventListener('click', openSidebar);
 
-function closeSidebar () {
-    sideBar.classList.remove('showSideBar');
+function closeSidebar() {
+  sideBar.classList.remove('showSideBar');
 }
 
 sideBarExit.addEventListener('click', closeSidebar);
