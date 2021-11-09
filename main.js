cca7f8718cd7222dea6186b67245b57c6545af57
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-11-09 11:30:36 +0300

    Modify main and style file to fulfilled the requirements

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const humberger = document.querySelector('.humberger');
 const sideBar = document.querySelector('.sideBar');
 const sideBarExit = document.querySelector('.sideBar-exit');
 const sideBarLink = document.querySelector('.sideBar-link');
+const about = document.getElementById('about-link');
+const contact = document.getElementById('contact-link');
 
 function openSidebar() {
   sideBar.classList.add('showSideBar');
@@ -15,3 +17,5 @@ function closeSidebar() {
 
 sideBarExit.addEventListener('click', closeSidebar);
 sideBarLink.addEventListener('click', closeSidebar);
+about.addEventListener('click', closeSidebar);
+contact.addEventListener('click', closeSidebar);
