c73e5e3c3d8083b8da3946b140564021c3f4b8ef
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2021-11-10 06:57:44 +0300

    Add html structure, picture popup and data.js file

---
+++ data.js
@@ -0,0 +1,44 @@
+const projects = [
+    {
+      id: 0,
+      title: 'Multi-Post stories',
+      picture: 'image/Img-Placeholder.png',
+      modalPicture: 'image/Img-Placeholder-Desktop.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
+      technologies: ['html', 'Ruby', 'css', 'bootstrap'],
+      liveLink: '#',
+      codeLink: '#',
+    },
+    {
+      id: 1,
+      title: 'Usb flash drive',
+      picture: 'image/Img-Placeholder.png',
+      modalPicture: 'image/Img-Placeholder-Desktop.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
+      technologies: [ 'html', 'node.js', 'bootstrap', 'sass',],
+      liveLink: '#',
+      codeLink: '#',
+    },
+    {
+      id: 2,
+      title: 'Facebook 360',
+      picture: 'image/Img-Placeholder.png',
+      modalPicture: 'image/Img-Placeholder-Desktop.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
+      technologies: [ 'html', 'node.js', 'bootstrap', 'sass',],
+      liveLink: '#',
+      codeLink: '#',
+    },
+    {
+      id: 3,
+      title: 'Uber Navigation',
+      picture: 'image/Img-Placeholder.png',
+      modalPicture: 'image/Img-Placeholder-Desktop.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe..",
+      technologies: [ 'html', 'node.js', 'bootstrap', 'sass'],
+      liveLink: '#',
+      codeLink: '#',
+    },
+  ];
+
+export default data;
